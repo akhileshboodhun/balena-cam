dcad336eac8e0bee76be80c3e11865f591682785
Akhilesh Boodhun <quantum19992011@gmail.com>
2021-07-17 22:29:46 +0400

    Update pivideostream.py

--- balena-cam/app/utils/pivideostream.py
+++ balena-cam/app/utils/pivideostream.py
@@ -12,6 +12,7 @@ class PiVideoStream:
         self.camera.resolution = resolution
         self.camera.framerate = framerate
         self.camera.led = False
+        self.camera.exposure_mode = 'night'
 
         # set optional camera parameters (refer to PiCamera docs)
         for (arg, value) in kwargs.items():
